4123ae449a2199cd5a8bcd9ab053ed1dff3febc3
Venu <venuskanda2005@gmail.com>
2020-11-27 09:34:29 +0530

    GreenLeafz 1.0

--- app/src/main/java/com/revno/greenleafz/Splash.java
+++ app/src/main/java/com/revno/greenleafz/Splash.java
@@ -10,7 +10,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Splash extends AppCompatActivity{
-    private static int SPLASH_SCREEN_TIME_OUT=2000;
+    private static int SPLASH_SCREEN_TIME_OUT = 2000;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
